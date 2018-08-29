06dba40d75c37c322bfff5ed0d9d62290fe29bde
s17002 <s17002@std.it-college.ac.jp>
2018-08-29 14:09:40 +0900

    0717

--- 0717/OpenHash.java
+++ 0717/OpenHash.java
@@ -1,140 +1,140 @@
 // オープンアドレス法によるハッシュ
 
-public class openhash<k,v> {
+public class OpenHash<K,V> {
 
 	//--- バケットの状態 ---//
-	enum status {occupied, empty, deleted};	// {データ格納, 空, 削除ずみ}
+	enum Status {OCCUPIED, EMPTY, DELETED};	// {データ格納, 空, 削除ずみ}
 
 	//--- バケット ---//
-	static class bucket<k,v> {
-		private k key;						// キー値
-		private v data;					// データ
-		private status stat;				// 状態
+	static class Bucket<K,V> {
+		private K key;						// キー値
+		private V data;					// データ
+		private Status stat;				// 状態
 
 		//--- コンストラクタ ---//
-		bucket() {
-			stat = status.empty;	// バケットは空
+		Bucket() {
+			stat = Status.EMPTY;	// バケットは空
 		}
 
 		//--- 全フィールドに値を設定 ---//
-		void set(k key, v data, status stat) {
+		void set(K key, V data, Status stat) {
 			this.key  = key;			// キー値
 			this.data = data;			// データ
 			this.stat = stat;			// 状態
 		}
 
 		//--- 状態を設定 ---//
-		void setstat(status stat) {
+		void setStat(Status stat) {
 			this.stat = stat;
 		}
 
 		//--- キー値を返す ---//
-		k getkey() {
+		K getKey() {
 			return key;
 		}
 
 		//--- データを返す ---//
-		v getvalue() {
+		V getValue() {
 			return data;
 		}
 
 		//--- キーのハッシュ値を返す ---//
-		public int hashcode() {
-			return key.hashcode();
+		public int hashCode() {
+			return key.hashCode();
 		}
 	}
 
 	private int size;						// ハッシュ表の大きさ
-	private bucket<k,v>[] table;		// ハッシュ表
+	private Bucket<K,V>[] table;		// ハッシュ表
 
 	//--- コンストラクタ ---//
-	public openhash(int size) {
+	public OpenHash(int size) {
 		try {
-			table = new bucket[size];
+			table = new Bucket[size];
 			for (int i = 0; i < size; i++)
-				table[i] = new bucket<k,v>();
+				table[i] = new Bucket<K,V>();
 			this.size = size;
-		} catch (outofmemoryerror e) {		// 表を生成できなかった
+		} catch (OutOfMemoryError e) {		// 表を生成できなかった
 			this.size = 0;
 		}
 	}
 
 	//--- ハッシュ値を求める ---//
-	public int hashvalue(object key) {
-		return key.hashcode() % size;
+	public int hashValue(Object key) {
+		return key.hashCode() % size;
 	}
 
 	//--- 再ハッシュ値を求める ---//
-	public int rehashvalue(int hash) {
+	public int rehashValue(int hash) {
 		return (hash + 1) % size;
 	}
 
 	//--- キー値keyをもつバケットの探索 ---//
-	private bucket<k,v> searchnode(k key) {
-		int hash = hashvalue(key);			// 探索するデータのハッシュ値
-		bucket<k,v> p = table[hash];		// 着目バケット
+	private Bucket<K,V> searchNode(K key) {
+		int hash = hashValue(key);			// 探索するデータのハッシュ値
+		Bucket<K,V> p = table[hash];		// 着目バケット
 
-		for (int i = 0; p.stat != status.empty && i < size; i++) {
-			if (p.stat == status.occupied && p.getkey().equals(key))
+		for (int i = 0; p.stat != Status.EMPTY && i < size; i++) {
+			if (p.stat == Status.OCCUPIED && p.getKey().equals(key))
 				return p;
-			hash = rehashvalue(hash);		// 再ハッシュ
+			hash = rehashValue(hash);		// 再ハッシュ
 			p = table[hash];
 		}
 		return null;
 	}
 
 	//--- キー値keyをもつ要素の探索（データを返却）---//
-	public v search(k key) {
-		bucket<k,v> p = searchnode(key);
+	public V search(K key) {
+		Bucket<K,V> p = searchNode(key);
 		if (p != null)
-			return p.getvalue();
+			return p.getValue();
 		else
 			return null;
 	}
 
 	//--- キー値key・データdataをもつ要素の追加 ---//
-	public int add(k key, v data) {
+	public int add(K key, V data) {
 		if (search(key) != null)
 			return 1;							// このキー値は登録ずみ
 
-		int hash = hashvalue(key);			// 追加するデータのハッシュ値
-		bucket<k,v> p = table[hash];		// 着目バケット
+		int hash = hashValue(key);			// 追加するデータのハッシュ値
+		Bucket<K,V> p = table[hash];		// 着目バケット
 		for (int i = 0; i < size; i++) {
-			if (p.stat == status.empty || p.stat == status.deleted) {
-				p.set(key, data, status.occupied);
+			if (p.stat == Status.EMPTY || p.stat == Status.DELETED) {
+				p.set(key, data, Status.OCCUPIED);
 				return 0;
 			}
-			hash = rehashvalue(hash);		// 再ハッシュ
+			hash = rehashValue(hash);		// 再ハッシュ
 			p = table[hash];
 		}
 		return 2;								// ハッシュ表が満杯
 	}
 
 	//--- キー値keyをもつ要素の削除 ---//
-	public int remove(k key) {
-		bucket<k,v> p = searchnode(key);	// 着目バケット
+	public int remove(K key) {
+		Bucket<K,V> p = searchNode(key);	// 着目バケット
 		if (p == null)
 			return 1;							// このキー値は登録されていない
 
-		p.setstat(status.deleted);
+		p.setStat(Status.DELETED);
 		return 0;
 	}
 
 	//--- ハッシュ表をダンプ ---//
 	public void dump() {
 		for (int i = 0; i < size; i++) {
-			system.out.printf("%02d ", i);
+			System.out.printf("%02d ", i);
 			switch (table[i].stat) {
-			 case occupied :
-				system.out.printf("%s (%s)\n",
-										table[i].getkey(), table[i].getvalue());
+			 case OCCUPIED :
+				System.out.printf("%s (%s)\n",
+										table[i].getKey(), table[i].getValue());
 				break;
 
-			 case empty :
-			 	system.out.println("-- 未登録 --");	break;
+			 case EMPTY :
+			 	System.out.println("-- 未登録 --");	break;
 
-			 case deleted :
-			 	system.out.println("-- 削除ずみ --");	break;
+			 case DELETED :
+			 	System.out.println("-- 削除ずみ --");	break;
 			}
 		}
 	}
