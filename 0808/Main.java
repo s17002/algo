17a16db8539465338ff7c7814840229836da75a9
s17002 <s17002@std.it-college.ac.jp>
2018-08-30 14:40:37 +0900

    0808

--- 0808/Main.java
+++ 0808/Main.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 
-class QuickSortEx1A {
+class Main {
 
-   //--- 配列の要素a[idx1]とa[idx2]を交換 ---//
    static void swap(int[] a, int idx1, int idx2) {
       int t = a[idx1];  a[idx1] = a[idx2];  a[idx2] = t;
    }
 
-   //--- 配列を分割する ---//
    static void quickSort(int[] a, int left, int right) {
-      int pl = left;               // 左カーソル
-      int pr = right;            // 右カーソル
-      int x = a[(pl + pr) / 2];   // 枢軸（中央の要素）
+      int pl = left;
+      int pr = right;
+      int x = a[(pl + pr) / 2];
 
       do {
          while (a[pl] < x) pl++;
@@ -42,10 +40,12 @@ class QuickSortEx1A {
          x[i] = stdIn.nextInt();
       }
 
-      quickSort(x, 0, nx - 1);         // 配列xをクイックソート
+      quickSort(x, 0, nx - 1);
 
       System.out.println("昇順にソートしました。");
       for (int i = 0; i < nx; i++)
          System.out.println("x[" + i + "]＝" + x[i]);
    }
 }
+
+
